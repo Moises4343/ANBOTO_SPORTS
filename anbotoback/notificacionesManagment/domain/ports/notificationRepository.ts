178a1e9fd7dd162ff62733ffbcb2fa2e557bae27
Alfredo-Borraz <69995433+Alfredo-Borraz@users.noreply.github.com>
2024-11-29 18:13:58 -0600

    deleted

--- anbotoback/notificacionesManagment/domain/ports/notificationRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Notification } from "../entities/Notification";
-
-export interface NotificationRepostory {
-    creataNotification(notification: Notification): Promise<any>;
-    getNotifications(playerUUID: string): Promise<Notification[]>;
-    deleteNotification(uuid: string) : Promise<void>;
-}
